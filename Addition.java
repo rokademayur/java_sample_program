ca19f9f0ca9d2d44f1405ede2135bfb5d300813b
Mayur <mayurrokade610@gmail.com>
2022-12-25 14:42:54 +0530

    addition

--- Addition.java
+++ Addition.java
@@ -1,5 +1,9 @@
 public class Addition {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
     }
 }
